82867a75c2da0f7e642a6a7db78e2a73955e132d
vijay-webcom <vijay@vsmartinfotech.net>
2026-01-21 13:50:48 +0530

    added favicon and meta data

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'IT Services And Solutions For the Financial Industry',
+  description: 'Webcom Systems offers innovative IT services &amp; solutions tailored for the financial industry, enhancing operational efficiency and security.',
+};
 export default function Home() {
   return (
     <>
